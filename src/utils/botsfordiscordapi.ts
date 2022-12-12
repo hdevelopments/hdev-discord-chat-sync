6481b6eb96388b1174ec3f220bc26c882874094b
Philipp Herz <34943569+Pdzly@users.noreply.github.com>
2022-12-12 10:44:18 +0100

    fixed discords api call

--- src/utils/botsfordiscordapi.ts
+++ src/utils/botsfordiscordapi.ts
@@ -5,7 +5,7 @@ import bot from "../main"
 class discordsApi {
     async syncUp(){
         if(!Config.Discords_Api_Token) return
-        axios.post("https://discords.com/bots/api/bot/" + bot.botId, {server_count: bot.guilds.cache.size}, {headers: {
+        axios.post("https://discords.com/bots/api/bot/" + bot.user?.id, {server_count: bot.guilds.cache.size}, {headers: {
             Authorization: Config.Discords_Api_Token
         }}).then(x =>{
             return x.data
