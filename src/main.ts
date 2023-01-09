175934f011ed46f85fdd9ed81bd86bdfc4e9d7d4
Philipp Herz <34943569+Pdzly@users.noreply.github.com>
2023-01-09 16:10:24 +0100

    added rpc to it?

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import Config from "./discordConfig";
 import { NoBot } from "./discord/guards/noBots";
 import { Container, Service } from "typedi";
 import discordsApi from "./utils/botsfordiscordapi";
-import { Typing } from "discord.js";
 import express from "express";
 import { apirouter } from "./express/api/api";
+import * as dcRP from "discord-rich-presence"
 
 var discords = new discordsApi();
 DIService.engine = typeDiDependencyRegistryEngine
@@ -134,6 +134,16 @@ var App = express();
 App.use("/api", apirouter);
 
 App.listen(3000);
+const rpcClient = dcRP.default("1046756800260735058")
+rpcClient.updatePresence({
+  state: "slithering",
+  details: "🐍",
+  startTimestamp: Date.now(),
+  endTimestamp: Date.now() + 1337,
+  largeImageKey: "hdevrainbow",
+  smallImageKey: "hdevorange",
+  instance: true,
+});
 
 export default bot;
 export { App };
