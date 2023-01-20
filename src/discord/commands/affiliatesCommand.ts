895800dfa0a27bcedb84ace281c684d57a96fce6
Philipp Herz <Philipp.Herz@agcocorp.com>
2023-01-20 12:03:19 +0100

    add timezone affiliate

--- src/discord/commands/affiliatesCommand.ts
+++ src/discord/commands/affiliatesCommand.ts
@@ -19,6 +19,15 @@ const affiliates = [
     image:
       "https://cdn.discordapp.com/attachments/816811551079923763/982460251506880552/BytesToBitsBanner.png",
   },
+  {
+    name: "Timezones Bot",
+    description: `The only bot you need to play with friends in a different timezone!
+    
+    🔗 Bot-Invite: https://discord.com/api/oauth2/authorize?client_id=1023653781398884431&permissions=66624&scope=bot%20applications.commands`,
+    invite: "https://discord.gg/pvBzwQur7M",
+    image:
+      "https://cdn.discordapp.com/attachments/1053061855876222976/1065933915073806336/image.png",
+  },
 ];
 
 @Discord()
@@ -28,27 +37,27 @@ class affiliatesCommand {
   })
   async affiliates(interaction: CommandInteraction) {
     await interaction.deferReply({ ephemeral: true });
-    var embeds: EmbedBuilder[] = [];
-    affiliates.forEach((x) => {
+    await interaction.editReply("Here are my Affiliates:")
+    affiliates.forEach(async (x) => {
       var embed = new EmbedBuilder();
       embed.setTitle(x.name);
       embed.setDescription(x.description);
       embed.setImage(x.image);
 
-      embeds.push(embed);
-    });
-
-    const affiliateBtn = new ButtonBuilder()
-      .setLabel("Check out BytesToBits!")
+      const affiliateBtn = new ButtonBuilder()
+      .setLabel(`Check out ${x.name}!`)
       .setStyle(ButtonStyle.Link)
-      .setURL("https://discord.gg/bytestobits-coding-614895425639546881");
-    var row =
+      .setURL(x.invite);
+
+      var row =
       new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
         affiliateBtn
       );
-    await interaction.editReply({
-      embeds: embeds,
-      components: [row],
+      await interaction.followUp({
+        embeds: [embed],
+        components: [row],
+        ephemeral: true
+      });
     });
   }
 }
