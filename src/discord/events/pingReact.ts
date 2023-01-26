8c7f9f035b6cc2d57931cfb900e2455587e59e63
Philipp Herz <Philipp.Herz@agcocorp.com>
2023-01-26 17:41:58 +0100

    fixed react

--- src/discord/events/pingReact.ts
+++ src/discord/events/pingReact.ts
@@ -14,55 +14,57 @@ import Logs from "../../utils/Logs";
 export class pingReact {
   @On({ event: "messageCreate" })
   async checkMessages([message]: ArgsOf<"messageCreate">) {
-    if(message.reference?.messageId){
-      var ref = await message.fetchReference()
-      if(ref.author.id == bot.user?.id){
-        return
+    try {
+      if (message.reference?.messageId) {
+        var ref = await message.fetchReference();
+        if (ref.author.id == bot.user?.id) {
+          return;
+        }
       }
-    }
-    if (message.mentions.has(bot.user!)) {
-      var embed = new EmbedBuilder();
-      embed.setAuthor({
-        name: message.member?.displayName || message.author.username,
-        iconURL:
-          message.member?.avatarURL() ||
-          message.author.avatarURL() ||
-          message.author.defaultAvatarURL,
-      });
-      embed.setColor(Colors.Gold);
+      if (message.mentions.has(bot.user!)) {
+        var embed = new EmbedBuilder();
+        embed.setAuthor({
+          name: message.member?.displayName || message.author.username,
+          iconURL:
+            message.member?.avatarURL() ||
+            message.author.avatarURL() ||
+            message.author.defaultAvatarURL,
+        });
+        embed.setColor(Colors.Gold);
 
-      embed.addFields({
-        name: "Creator",
-        value: "<@710213572898193428>",
-        inline: true,
-      });
-      embed.addFields({
-        name: "Support Server",
-        value: "https://discord.gg/hdev",
-        inline: true,
-      });
-      embed.addFields({
-        name: "Help Command:",
-        value: "</info:1060278568447398009>",
-        inline: true,
-      });
-      const shareBtn = new ButtonBuilder()
-        .setLabel("Invite the Bot!")
-        .setStyle(ButtonStyle.Link)
-        .setURL(
-          "https://discord.com/api/oauth2/authorize?client_id=1046756800260735058&permissions=533113203777&scope=bot%20applications.commands"
-        );
-      const joinSupport = new ButtonBuilder()
-        .setLabel("Join the Support Server!")
-        .setStyle(ButtonStyle.Link)
-        .setURL("https://discord.gg/hdev");
-      var row =
-        new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-          shareBtn,
-          joinSupport
-        );
-      await message.reply({ embeds: [embed], components: [row] });
-    }
+        embed.addFields({
+          name: "Creator",
+          value: "<@710213572898193428>",
+          inline: true,
+        });
+        embed.addFields({
+          name: "Support Server",
+          value: "https://discord.gg/hdev",
+          inline: true,
+        });
+        embed.addFields({
+          name: "Help Command:",
+          value: "</info:1060278568447398009>",
+          inline: true,
+        });
+        const shareBtn = new ButtonBuilder()
+          .setLabel("Invite the Bot!")
+          .setStyle(ButtonStyle.Link)
+          .setURL(
+            "https://discord.com/api/oauth2/authorize?client_id=1046756800260735058&permissions=533113203777&scope=bot%20applications.commands"
+          );
+        const joinSupport = new ButtonBuilder()
+          .setLabel("Join the Support Server!")
+          .setStyle(ButtonStyle.Link)
+          .setURL("https://discord.gg/hdev");
+        var row =
+          new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+            shareBtn,
+            joinSupport
+          );
+        await message.reply({ embeds: [embed], components: [row] });
+      }
+    } catch {}
   }
 
   @On({ event: "guildCreate" })
